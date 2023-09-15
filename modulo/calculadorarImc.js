90da6f964a33e835ee001d468fd84b6f4319ba74
Maria Luiza <maria.luiza.costa.balieiro@gmail.com>
2023-09-15 17:19:47 -0300

    em andamento

---
+++ modulo/calculadorarImc.js
@@ -0,0 +1,17 @@
+// ********************************************************************************
+// objetivo: realizar ativiadaes afim de colocar os conhecimentos em pratica      *
+// data: 15/09/2023                                                               *
+// autor: Maria Luiza                                                             *
+// vrsão: 1.0                                                                     *
+// ********************************************************************************
+
+
+const geraraIMC = function(valorAutura, valorPeso, resultadoImc){
+
+    let autura = valorAutura;
+    let peso = valorPeso;
+    let resutado = resultadoImc;
+}
+
+
+
